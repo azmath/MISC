8aa72696db46605fedb3c892eabca0fb6aaf8d9a
mutex <iamazmath@gmail.com>
2022-05-31 08:20:55 +0400

    Update DB-triggers.sql

--- DB-triggers.sql
+++ DB-triggers.sql
@@ -15,3 +15,7 @@ end if;
 END;//
 DELIMITER ;
 
+
+ALTER TABLE `issues` CHANGE `date_updated` `date_updated` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+
+ALTER TABLE `issues` ADD `date_closed` DATE;
